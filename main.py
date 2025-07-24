8e4908d21d605f34d737363ed701d5350b195cde
Alfred <amalinga@student.wethinkcode.co.za>
2025-07-24 13:30:44 +0200

    Added the add & show task functionality on the listed tasks.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+lstToDo =[]
 while True:
     print("Let's help with your TO DOs")
     print("""
@@ -9,7 +10,19 @@ while True:
     Enter-  Delete TaskNo -to delete a task
     Enter- Exit -to exit the app\n""")
     promt = input("What do you want to do:")
-    if promt.__contains__("Exit"):
+
+
+    if promt.__contains__("Add"):
+        task = input("Enter your task: ")
+        lstToDo.append(task)
+
+    elif promt.__contains__("Show"):
+        print("These a the task that are listed:")
+        for index in lstToDo:
+            print(index)
+
+    elif promt.__contains__("Exit"):
         break
+
 
 
